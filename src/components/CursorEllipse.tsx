f1077dadae5f36a003fbf83916ffa6186fcc1528
Giu <mail@giualfeo.com>
2024-11-09 21:41:31 +0000

    Display of all active user cursors

--- src/components/CursorEllipse.tsx
+++ src/components/CursorEllipse.tsx
@@ -1,31 +1,53 @@
+
 import React, { useState, useEffect } from 'react';
+import { useStateTogetherWithPerUserValues } from 'react-together';
+
+// const test = {
+//   a: 'aaaa',
+//   b: 'bbbb',
+//   c: 'ccccc'
+// }
+
+// function testtttt(test) {
+//   const { a, b } = test.a.aa.aaa
+
+//   console.log(a)
+//   console.log(test.a)
+// }
 
 const CursorEllipse = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [, setCursorPositions, cursorPositions] = useStateTogetherWithPerUserValues('cursor', {x: 0, y:0});
 
-  useEffect(() => {
-    const handleMouseMove = (e) => {
+  React.useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
       // Update cursor position
-      setPosition({ x: e.clientX, y: e.clientY });
+      setCursorPositions({ x: e.clientX, y: e.clientY });
     };
 
     // Add event listener to track mouse movement
     window.addEventListener('mousemove', handleMouseMove);
-
+    
     return () => {
       // Clean up the event listener on component unmount
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, []);
+  }, [setCursorPositions]);
 
   return (
-    <div
-      className="ellipse"
-      style={{
-        left: position.x,
-        top: position.y,
-      }}
-    />
+    <div>
+      {/* Render an ellipse for each user in the `cursorPositions` object */}
+        {Object.entries(cursorPositions).map(([userId, position]) => (
+        <div
+          key={userId}
+          className="ellipse"
+          style={{
+            left: position.x,
+            top: position.y,
+            backgroundColor: userId === 'your-user-id' ? 'blue' : 'black', // Optional: Different color for local user
+          }}
+        />
+      ))}
+    </div>
   );
 };
 
