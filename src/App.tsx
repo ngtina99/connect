2845f90b6f3f6a1798a40c11baccbbc179d9bdc9
Tina <nguyen.valentina.99@gmail.com>
2024-11-10 01:06:21 +0100

    fixed static background

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.scss'
 import '@styles/examplePage.scss'
 
 import { useStateTogether } from 'react-together'
-import Map from './components/Map';
+import Map from './components/Background';
 import Navbar from '@components/Navbar';
 import Main from './components/Main';
 
@@ -16,22 +16,13 @@ export default function App() {
   return (
     <div>
       <Navbar/>
-      <div>
+      {/* <div>
         <HeroLogo {...{ type: 'reacttogether' }} />
         <HeroLogo {...{ type: 'react' }} />
         <HeroLogo {...{ type: 'vite' }} />
-      </div>
+      </div> */}
       <CursorEllipse />
-      {/* <Main />  */}
-	  <h1>My Google Map</h1>
       <Map />
-      <div className='card'>
-        <button onClick={() => set_count((count) => count + 1) }>Synq'd count is {count}</button>
-        {/* <button onClick={() => set_count((count) => count + 1)}>Synq'd count is {count}</button> */}
-        <button {...{ style: { marginLeft: '1rem' }, onClick: () => set_count(0) }}>Reset</button>
-      </div>
-      <p className='read-the-docs'>Click on the respective logos to learn more.</p>
-
       <div className='version-num'>{version}</div>
     </div>
   )
---
+++ src/components/Background.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/Background.scss';
+
+const Background: React.FC = () => {
+  return <div className="background-container"></div>;
+};
+
+export default Background;
