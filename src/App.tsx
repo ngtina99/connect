880c74a843d58871464e2dcdb8346c9f52ac96cb
Tina <nguyen.valentina.99@gmail.com>
2024-11-10 12:47:34 +0100

    added link+

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import CursorEllipse from './components/CursorEllipse';
 import JoinSessionButton from './components/JoinSessionButton.tsx';
 import RippleCircle from '@components/RipleCircle/RipleCircle';
 import './gobalStyle.css'
+import SessionComponent from './components/SessionComponent'; // Adjust the path based on where you put it
 
 export default function App() {
   return (
@@ -26,7 +27,7 @@ export default function App() {
               
             </Routes>
 			</div>
-
+			<SessionComponent />
 {/* JoinSessionButton (appears on all pages) */}
 {/* <JoinSessionButton /> */}
 </div>
